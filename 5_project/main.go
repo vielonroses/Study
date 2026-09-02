c437cc5aa8c96883db6bc168080e70cc5390ffd8
артем <vielon@vielonroses.local>
2026-09-02 22:18:27 +0300

    update 5_project

--- 5_project/main.go
+++ 5_project/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"math/rand"
 	"os"
 )
@@ -11,6 +12,7 @@ func main() {
 	fmt.Println("Parodia on linux 'random' haha ")
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
+		fmt.Println("")
 		fmt.Print("sh 5.0 $: ")
 		scanner.Scan()
 		cmd := scanner.Text()
@@ -19,13 +21,23 @@ func main() {
 			fmt.Println("Command:")
 			fmt.Println("-	random")
 			fmt.Println("-	exit")
+			fmt.Println("-	Pi")
+			fmt.Println("-	square")
 		case "random":
 			fmt.Println(rand.Intn(100000))
 		case "exit":
 			return
+		case "Pi":
+			fmt.Printf("Pi: %g", math.Pi)
+		case "square":
+			var x float64
+			fmt.Print("Enter ur number: ")
+			fmt.Scanln(&x)
+			fmt.Printf("Square root: %g", math.Sqrt(x))
+
 		default:
 			if cmd != "" {
-				fmt.Println("command not found, please try again")
+				fmt.Println("command not found, please try again.")
 			}
 		}
 	}
